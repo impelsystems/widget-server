4b7dbcd7fdc6834d94c5363a2f75606e5400dc3e
Paul Stephenson <paul@impel.co>
2025-02-28 09:56:41 +1100

    Db, models, services to context

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,30 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { schema as typeDefs } from "./schemas";
 import { resolvers } from "./resolvers";
+import { createModels } from "./models";
+import { createServices } from "./services";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const config: any = dotenv.config().parsed;
+
+const db = await mongoose.createConnection(config.DB_URI);
+
+const models = createModels(db);
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection: true,
 });
 
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: config.PORT },
+  context: async () => {
+    return {
+      services: createServices(models),
+    };
+  },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
